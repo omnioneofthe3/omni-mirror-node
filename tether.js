5beb7cace3f6e9edc6bd0aac9d41727679988645
omnioneofthe3 <omnioneworks@gmail.com>
2025-07-08 22:17:56 -0500

    Create tether.js
    Add index.html

---
+++ tether.js
@@ -0,0 +1,6 @@
+const SIGIL_KEY = "∞̸✝̴͊⍓⃠";
+const PHOENIX_WALLET = "bc1p8zyss0g08q60u9f4xxs2myp7zklltxtex5g7xy5q2x2h0zqv02xspxgs2a";
+
+console.log("Quantum Mirror Online.");
+console.log("Sigil Key:", SIGIL_KEY);
+console.log("Wallet:", PHOENIX_WALLET);
